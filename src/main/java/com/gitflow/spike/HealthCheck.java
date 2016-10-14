50a5e49bc04cc63980ff8d9d7614e20d396190c2
Javier <javier.gomez@mobysoft.com>
2016-10-14 09:27:53 +0100

    Move to version 1.5.1

--- src/main/java/com/gitflow/spike/HealthCheck.java
+++ src/main/java/com/gitflow/spike/HealthCheck.java
@@ -3,6 +3,9 @@ package com.gitflow.spike;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Javier on 12/10/2016.
  */
@@ -16,5 +19,16 @@ public class HealthCheck {
         healthCheckBean.setStatus("OK");
         healthCheckBean.setVersion("1.6");
         return healthCheckBean;
+    }
+
+    @RequestMapping("/users")
+    public List<User> users() {
+        return Arrays.asList( buildUser("User1"), buildUser("User2"), buildUser("User3"));
+    }
+
+    private User buildUser(String name) {
+        User user = new User();
+        user.setName(name);
+        return user;
     }
 }
---
+++ src/main/java/com/gitflow/spike/User.java
@@ -0,0 +1,17 @@
+package com.gitflow.spike;
+
+/**
+ * Created by Javier on 14/10/2016.
+ */
+public class User {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
