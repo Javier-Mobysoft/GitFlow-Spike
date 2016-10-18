777e93e156c5566d0cd02096670410c3267e0149
Javier <javier.gomez@mobysoft.com>
2016-10-18 16:21:12 +0100

    Added sources and javadoc

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 version=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep  -v INFO`
 echo $version
-version=`echo $version | sed "s/\([0-9][0-9]*-SNAPSHOTS\)/${GO_PIPELINE_COUNTER}${BRANCH_ENV}/g"`
+version=`echo $version | sed "s/\([0-9][0-9]*-SNAPSHOT\)/${GO_PIPELINE_COUNTER}${BRANCH_ENV}/g"`
 echo $version
 mvn versions:set -DnewVersion=$version
