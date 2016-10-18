fe3899a33f4fb1e19af6bd2bf1eee95c07aec513
Javier <javier.gomez@mobysoft.com>
2016-10-18 09:39:58 +0100

    Added script to promote

--- scripts/promote.sh
+++ scripts/promote.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-export VERSION=`echo ${GO_PIPELINE_LABEL} | awk '{split($0,a,"."); print a[2
-]}'`
+export VERSION=`echo ${GO_PIPELINE_LABEL} | awk '{split($0,a,"."); print a[2]}'`
 mvn deploy:deploy-file -Dfile=demo.war -DrepositoryId=releaseRepo -Durl=http://192.168.33.100:8081/nexus/content/repositories/releases  -DgroupId=com.gitflow.spike -DartifactId=demo -Dversion=${VERSION}
