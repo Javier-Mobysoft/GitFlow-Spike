7837698eb805d0d4cc17c591091bfb112e58518f
Javier <javier.gomez@mobysoft.com>
2016-10-19 13:25:50 +0100

    Use maven versioning

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 version=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep  -v INFO`
 incremental_version=`echo ${GO_PIPELINE_LABEL} | awk '{split($0,a,"."); print a[2]}'`
-version=`echo $version | sed "s/\([0-9][0-9]*-SNAPSHOT\)/${incremental_version}${BRANCH_ENV}/g"`
+version=`echo $version | sed "s/-SNAPSHOT/${BRANCH_ENV}-SNAPSHOT/g"`
 mvn versions:set -DnewVersion=$version
