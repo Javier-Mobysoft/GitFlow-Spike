71e7ec82c100e656b423ffd32e014804ff264736
Javier <javier.gomez@mobysoft.com>
2016-10-19 15:57:33 +0100

    Rollback to old build number solution

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-version=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep  -v INFO`
 incremental_version=`echo ${GO_PIPELINE_LABEL} | awk '{split($0,a,"."); print a[2]}'`
-version=`echo $version | sed "s/-SNAPSHOT/${BRANCH_ENV}-SNAPSHOT/g"`
-mvn versions:set -DnewVersion=$version
+mvn versions:set -DnewVersion=${BRANCH_ENV}-${incremental_version}
