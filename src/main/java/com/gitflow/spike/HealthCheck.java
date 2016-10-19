53868c6634fa2d81bf52191bbc1c7ebb7ef1807e
Javier <javier.gomez@mobysoft.com>
2016-10-19 11:47:55 +0100

    Added Versioning from maven

--- src/main/java/com/gitflow/spike/HealthCheck.java
+++ src/main/java/com/gitflow/spike/HealthCheck.java
@@ -17,7 +17,7 @@ public class HealthCheck {
     public HealthCheckBean check() {
         HealthCheckBean healthCheckBean = new HealthCheckBean();
         healthCheckBean.setStatus("OK");
-        healthCheckBean.setVersion("1.8.4");
+        healthCheckBean.setVersion(this.getClass().getPackage().getImplementationVersion());
         return healthCheckBean;
     }
 
