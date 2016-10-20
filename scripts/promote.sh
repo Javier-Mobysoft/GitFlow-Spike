41d0e98aa7f4166b7ca8a02dde740c03d3139e1d
Javier <javier.gomez@mobysoft.com>
2016-10-20 12:38:41 +0100

    Added git revision on artifact name

--- scripts/promote.sh
+++ scripts/promote.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export VERSION=`echo ${GO_PIPELINE_LABEL} | awk '{split($0,a,"."); print a[2]}'`
-mvn deploy:deploy-file -Dfile=demo.war -DrepositoryId=releaseRepo -Durl=http://192.168.33.200:8081/nexus/content/repositories/releases  -DgroupId=com.gitflow.spike -DartifactId=demo -Dversion=${VERSION} -Djavadoc=demo-javadoc.jar -Dsources=demo-sources.jar
+incremental_version=`echo ${GO_PIPELINE_LABEL} | awk '{split($0,a,"."); print a[2]}'`
+mvn versions:set -DnewVersion={incremental_version}
--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 incremental_version=`echo ${GO_PIPELINE_LABEL} | awk '{split($0,a,"."); print a[2]}'`
-mvn versions:set -DnewVersion=${BRANCH_ENV}${incremental_version}
+mvn versions:set -DnewVersion={incremental_version}-SANPSHOT
